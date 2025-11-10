87b30da3238dcf26e1dd6bb52b19e6a707e8ca80
Marselão <marcelo.lovato@escola.pr.gov.br>
2025-11-10 11:04:30 -0300

    adicionando var paciente [i]

--- js/principal.js
+++ js/principal.js
@@ -16,30 +16,32 @@ var pacientes = document.querySelectorAll ("paciente")
 
 for (var i=0; i  < pacientes.length; i++) {
 
-    var tdPeso =  paciente.querySelector(".info.peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
-//validar os dados de peso
-if (peso <= 0 ||  peso > 1000) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-//validar a altura
-if (altura <=0 || altura > 3){
-    console.log("Altura inválida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
-//validar peso e altura
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc.toFixed(2);
-  }
+    var tdPeso =  paciente.querySelector(".info.peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    //validar os dados de peso
+    if (peso <= 0 ||  peso > 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+    //validar a altura
+    if (altura <=0 || altura > 3){
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+    //validar peso e altura
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 
 }
