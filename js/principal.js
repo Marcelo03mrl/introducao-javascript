6fa832ce61e17ad450032656c7aa8cfd2c86c746
Marselão <marcelo.lovato@escola.pr.gov.br>
2025-11-10 10:51:41 -0300

    imc para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -2,9 +2,21 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 /*  IMC = massa/altura^2    */
-
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso =  document.querySelector(".info.peso");
+
+/*
+console.log(paciente);  //tr
+console.log(tdPeso);    // td que tem peso
+console.log(tdAltura);  //td que tem altura
+console.log(peso);      //valor do peso
+console.log(altura);    //valor da altura
+*/
+
+var pacientes = document.querySelectorAll ("paciente")
+
+for (var i=0; i  < pacientes.length; i++) {
+
+    var tdPeso =  document.querySelector(".info.peso");
 var peso = tdPeso.textContent;
 var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
@@ -28,10 +40,6 @@ if (altura <=0 || altura > 3){
 if (alturaEhValida && pesoEhValido) {
     var imc = peso / (altura*altura);
     tdImc.textContent = imc;
-}
+  }
 
-console.log(paciente);  //tr
-console.log(tdPeso);    // td que tem peso
-console.log(tdAltura);  //td que tem altura
-console.log(peso);      //valor do peso
-console.log(altura);    //valor da altura
+}
