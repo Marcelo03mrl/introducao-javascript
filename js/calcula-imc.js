5317dbf1836414df3b80c688feecbf94ee040e4a
Marcelo <marcelo.lovato@escola.pr.gov.br>
2025-12-10 13:56:21 -0300

    ajustes

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,8 +1,6 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-
-
 /*
 console.log(paciente);  //tr
 console.log(tdPeso);    // td que tem peso
@@ -11,11 +9,11 @@ console.log(peso);      //valor do peso
 console.log(altura);    //valor da altura
 */
 
-var pacientes  = document.querySelectorAll (".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
 for (var i=0; i < pacientes.length; i++) {
 
-    var paciente = pacientes[i];       //inserir
+    var paciente = pacientes[i];      // inserir
 
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
@@ -26,48 +24,45 @@ for (var i=0; i < pacientes.length; i++) {
     var pesoEhValido = validaPeso(peso);
     var alturaEhValida = validaAltura(altura);
     
-
     if (!pesoEhValido) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
-
     }
     
-    if (!alturaEhValida) {
+    if (!alturaEhValida){
         console.log("Altura inválida");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
-
     }
     
     if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso,altura);
         tdImc.textContent = imc;
     }
+
 }
 
+// Reescrever o mesmo código em tres funções independentes
 
- // Reescrever o mesmo código em três funções independentes
- function calculaImc(peso,altura){
+function calculaImc(peso, altura){
     var imc = 0;
     imc = peso/(altura * altura);
     return imc.toFixed(2);
+}
 
- }
-
- function validaPeso(peso){
-    if (peso <= 0 || peso>= 1000) {
-
+function validaPeso(peso){
+    if (peso <= 0 ||  peso >= 1000) {
+        return false;
     } else {
         return true;
     }
 }
 
- function validaAltura(altura) {
+function validaAltura(altura) {
     if (altura <=0 || altura > 3.00){
         return false;
     } else {
         return true;
     }
- }
+}
